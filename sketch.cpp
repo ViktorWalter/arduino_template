23f24ca0cd253096a2426aa27e3ded505f49839c
Viktor Walter <viktor_walter@outlook.com>
2022-02-17 02:26:16 +0100

    I have tested in a small scale a 4x2x2 (RxCxL) 3D matrix.

--- sketch.cpp
+++ sketch.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 
-#define rows 8 //X
+#define rows 4 //X
 #define columns 2 //Y
-#define layers 1 //Z
+#define layers 2 //Z
 
 #define lowbit_row 0 //pin of the lowest bit
 #define lowbit_column 0 //pin of the lowest bit
@@ -11,37 +11,51 @@
 /* int LED1 = 2; */
 
 /* unsigned char slider_row_start = 1; */
-unsigned char slider_row = 1;
+/* unsigned char slider_row = 1; */
 
 /* unsigned char slider_column_start = 1; */
-unsigned char slider_column = 1;
+/* unsigned char slider_column = 1; */
 
 /* unsigned char slider_layer_start = 1; */
-unsigned char slider_layer = 1;
+/* unsigned char slider_layer = 1; */
 
-unsigned char MSG[] = {B00100100, B00110000};
-unsigned char buffer[] = {B00000000, B00000000};
+typedef unsigned char matrix3D[layers][columns];
+
+/* matrix3D MSG = {{B10100101}, {B00110110}}; */
+/* matrix3D buffer = {{B00000000}, {B00000000}}; */
+matrix3D MSG = {{B00001010, B00000011}, {B00001110, B00001001}};
+matrix3D buffer = {{B00000000, B00000000}, {B00000000, B00000000}};
+/* matrix3D MSG = {B10100101, B00110110}; */
+/* matrix3D buffer = {{B00000000}, {B00000000}}; */
 
 unsigned int step = 0;
-unsigned int move_speed = 1000;
+unsigned int move_speed = 500;
+/* unsigned int move_speed = 0; */
 
 void setup() {
   /* pinMode(LED1,OUTPUT); */
-  DDRD = B11111111;
-  DDRB = B11111111;
+
+  /* DDRD = B11111111; */
+  DDRD = (((unsigned char)1)<<rows)-1;
+
+  DDRB = B00000001;
+
+  DDRC = B00000001;
 
   /* slider_row_start = slider_row_start << lowbit_row; */
-  slider_row = slider_row << lowbit_row;
+  /* slider_row = slider_row << lowbit_row; */
 
   /* slider_column_start = slider_column_start << lowbit_column; */
-  slider_column= slider_column<< lowbit_column;
+  /* slider_column= slider_column<< lowbit_column; */
   
   /* slider_layer_start  = slider_layer_start  << lowbit_layer; */
-  slider_layer = slider_layer << lowbit_layer;
+  /* slider_layer = slider_layer << lowbit_layer; */
   
 
-  for (int i=0; i<columns; i++){
-    buffer[i] = MSG[i];
+  for (int i=0; i<layers; i++){
+    for (int j=0; j<columns; j++){
+      buffer[i][j] = MSG[i][j];
+    }
   }
   //Initial setup
 }
@@ -51,36 +65,46 @@ unsigned char rotate(unsigned char input,int len){
 }
 
 
-void display_values(unsigned char* input){
+void display_values(matrix3D input){
   /* slider_layer = slider_layer_start; */
-  for (int k=0; k<layers; k++){
+  for (unsigned int k=0; k<layers; k++){
     /* slider_column = slider_column_start; */
-    for (int j=0; j<columns; j++){
-      PORTB = ~slider_column;
+    PORTC = k;
+    PORTB = 0;
+    for (unsigned int j=0; j<columns; j++){
+      /* PORTB = ~slider_column; */
+      /* PORTB = B11111111; */
       /* slider_row = slider_row_start; */
-      for (int i=0; i<rows; i++){
-        PORTD = slider_row & input[j];
-        delay(1);
-        slider_row = rotate(slider_row,rows);
-      }
-      slider_column = rotate(slider_column,columns);
+      /* for (int i=0; i<rows; i++){ */
+        /* PORTD = slider_row & input[j]; */
+        PORTD = input[k][j];
+        /* slider_row = rotate(slider_row,rows); */
+      /* } */
+      /* slider_column = rotate(slider_column,columns); */
+        PORTB = j+1;
+        /* delay(1); */
     }
-    slider_layer = rotate(slider_layer,layers);
+    delay(1);
+    /* slider_layer = rotate(slider_layer,layers); */
   }
 }
 
-void shift_message(unsigned char* input,int offset){
-  for (int i=0; i<columns; i++){
-    input[i] = rotate(input[i],rows);
+void shift_message(matrix3D input,int offset){
+  for (int i=0; i<layers; i++){
+    for (int j=0; j<columns; j++){
+      input[i][j] = rotate(input[i][j],rows);
+    }
   }
 }
 
 void loop() {
 
-  if ((millis()/move_speed) != step){
+  if (move_speed > 0){
+    if ((millis()/move_speed) != step){
     step = millis()/move_speed;
     int offset = step % rows;
     shift_message(buffer,offset);
+    }
   }
 
   display_values(buffer);
